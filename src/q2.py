94bc465694a862946a9b8c7a7e3e5ad85cef5629
Jenho88 <jenho88dummy2@gmail.com>
2026-06-08 11:42:35 +0800

    Update q2.py

--- src/q2.py
+++ src/q2.py
@@ -5,10 +5,17 @@ def find_and_replace(lst, find_val, replace_val):
     - lst must be a list.
     - Return the modified list.
     """
-    return
+    for x in range(len(lst)):
+        if lst[x] == find_val:
+            lst[x] = replace_val
+    return lst
 
 
 # Task 2
 # Invoke the function "find_and_replace" using the following scenarios:
 # - [1, 2, 3, 4, 2, 2], 2, 5
 # - ["apple", "banana", "apple"], "apple", "orange"
+tempLst = find_and_replace([1, 2, 3, 4, 2, 2], 2, 5)
+print(tempLst)
+tempLst2 = find_and_replace(["apple", "banana", "apple"], "apple", "orange")
+print(tempLst2)
